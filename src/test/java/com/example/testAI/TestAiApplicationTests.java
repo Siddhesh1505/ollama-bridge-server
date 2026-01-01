2297e726ea57f1dbf2f662dbc3ca1773729edec5
siddheshtoraskar <siddheshtoraskar1505@gmail.com>
2026-01-01 15:16:04 +0530

    feat : first commit

---
+++ src/test/java/com/example/testAI/TestAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
